09ccaf82c5e507ed577c0f45b34d12dc1e08ec72
Bilaal Yadallee <bilaal.yadallee@insign.fr>
2022-07-03 17:11:36 +0200

    [JAVA] Using new type of Coach

--- demo-one/src/com/ii02735/springdemo/Application.java
+++ demo-one/src/com/ii02735/springdemo/Application.java
@@ -4,7 +4,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		// Utilisation d'une interface
-		Coach coach = new BaseballCoach();
+		Coach coach = new TrackCoach();
 		// L'appel de la méthode ne dépend plus d'une classe mais d'une abstraction
 		System.out.println(coach.getDailyWorkout());
 	}
