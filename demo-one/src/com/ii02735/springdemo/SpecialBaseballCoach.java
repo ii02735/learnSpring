5315194a4d3db3148ff1acf6064cfac4e0179158
Bilaal Yadallee <bilaal.yadallee@insign.fr>
2022-07-03 17:50:02 +0200

    [JAVA] Adding another baseball coach type

---
+++ demo-one/src/com/ii02735/springdemo/SpecialBaseballCoach.java
@@ -0,0 +1,11 @@
+package com.ii02735.springdemo;
+
+public class SpecialBaseballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Spend 2 hours on batting practice";
+	}
+
+}
