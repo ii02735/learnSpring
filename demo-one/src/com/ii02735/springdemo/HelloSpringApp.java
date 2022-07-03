91be74a2a4a082d66462f0eba78d4a022f798d41
Bilaal Yadallee <bilaal.yadallee@insign.fr>
2022-07-03 17:44:39 +0200

    [SPRING] Core : application class with container invoking + config for showing logs

---
+++ demo-one/src/com/ii02735/springdemo/HelloSpringApp.java
@@ -0,0 +1,19 @@
+package com.ii02735.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+	
+	public static void main(String[] args) {
+		
+		// Chargement du fichier de configuration de spring
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		// Récupération du bean depuis le container
+		Coach coach = context.getBean("baseballCoach", Coach.class);
+		// Appeler les méthodes du bean
+		System.out.println(coach.getDailyWorkout());
+		// Fermer le contexte d'application
+		context.close();
+	}
+	
+}
