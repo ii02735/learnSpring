ab4af5de7fb35d1370645c8976577793762c9d96
ii02735 <yadallee.bilaal@outlook.fr>
2022-07-04 08:26:18 +0200

    [JAVA / SPRING] Application context updated for setter injection + CricketCoach class created

---
+++ demo-one/src/com/ii02735/springdemo/CricketCoach.java
@@ -0,0 +1,21 @@
+package com.ii02735.springdemo;
+
+public class CricketCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	public void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Practice fast bowling for 15 minutes";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return this.fortuneService.getFortune();
+	}
+
+}
