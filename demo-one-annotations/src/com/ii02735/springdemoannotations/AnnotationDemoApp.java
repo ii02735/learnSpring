d96253350d81caec0ebd8d084bf50c0d00cab403
ii02735 <yadallee.bilaal@outlook.fr>
2022-07-14 22:23:06 +0200

    [JAVA / SPRING] Using FortuneService dependency -> injection by method with @Autowired

--- demo-one-annotations/src/com/ii02735/springdemoannotations/AnnotationDemoApp.java
+++ demo-one-annotations/src/com/ii02735/springdemoannotations/AnnotationDemoApp.java
@@ -6,12 +6,20 @@ public class AnnotationDemoApp {
 	
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
 		Coach coach = context.getBean("tennisCoach",Coach.class);
 		System.out.println(coach.getDailyWorkout());
 		System.out.println("tennisCoach : " + coach.getDailyFortune());
+		
 		coach = context.getBean("basketCoach",Coach.class);
 		System.out.println(coach.getDailyWorkout());
 		System.out.println("basketCoach : " + coach.getDailyFortune());
+		
+		coach = context.getBean("cricketCoach",Coach.class);
+		System.out.println(coach.getDailyWorkout());
+		System.out.println("CricketCoach : " + coach.getDailyFortune());
+		
+		
 		context.close();
 	}
 }
---
+++ demo-one-annotations/src/com/ii02735/springdemoannotations/CricketCoach.java
@@ -0,0 +1,28 @@
+package com.ii02735.springdemoannotations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	// Utilisation de l'injection par méthode (plutôt que par mutateur)
+	@Autowired
+	private void initializeFortuneService(FortuneService fortuneService)
+	{
+		this.fortuneService = fortuneService;
+	}
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Strike the ball !";
+	}
+	
+	@Override
+	public String getDailyFortune() {
+		return this.fortuneService.getFortune();
+	}
+
+}
